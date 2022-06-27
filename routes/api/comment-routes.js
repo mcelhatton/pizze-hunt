2652f8b06620e743f81b8d2ca44f8549a559dc3d
mcelhatton <colinm@mobiledealerdata.com>
2022-06-27 15:08:07 -0500

    deploying to heroku

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -4,7 +4,7 @@ const {
   removeComment,
   addReply,
   removeReply
-} = require('../../controllers/comment-controller');
+} = require('../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
